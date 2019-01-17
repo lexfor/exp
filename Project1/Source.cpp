c424e63862e4539fd70ea302b4e5497c2b35c6ce
lexfor <thetim182001@mail.ru>
2019-01-17 10:36:52 +0300

    Добавьте файлы проекта.

---
+++ Project1/Source.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+class student {
+	private :
+		string name;
+		int math;
+		int phys;
+		int inf;
+		int chem;
+};
+int main() {
+
+	system("pause");
+	return 0;
+}
